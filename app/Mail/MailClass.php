328a1e72d2871c34274f293514d9bee6f46b7707
Анастасия Полякова <78700473+Poliakkova@users.noreply.github.com>
2021-05-18 23:15:37 +0300

    Add files via upload

---
+++ app/Mail/MailClass.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class MailClass extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $name;
+    protected $email;
+    protected $phone;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($name, $email, $phone)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->phone = $phone;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('email')
+            ->with([
+                'name' => $this->name,
+                'email' => $this->email,
+                'phone' => $this->phone,
+            ])
+            ->subject('Backstage');
+    }
+}
